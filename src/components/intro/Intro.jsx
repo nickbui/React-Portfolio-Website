9e0374a2a740779abffe8ed0f13dfbfa1cc09d71
nickbui <nicholas.t.bui@gmail.com>
2021-08-10 10:53:00 -0400

    Added type animation to intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Developer", "Designer"],
+        });
+    },[])
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -13,7 +27,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Nicholas Bui</h1>
-                    <h3>Frontend Developer</h3>
+                    <h3>Front-end <span ref={ textRef }></span></h3>
                 </div>
             </div>
         </div>
